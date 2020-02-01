85e2a719cf73f79f0365a9cfad680fe1854948f3
Kyra Mellema <kemellema@gmail.com>
2020-01-31 21:08:53 -0500

    Practice from 1/31/20

---
+++ codecademyJava.java
@@ -0,0 +1,15 @@
+// “Write once, run everywhere”
+
+// Run the code in the text editor to see what is printed to the screen.
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
+
+// Inside main(), add a statement which prints Hello someName!, with your name replacing someName.
+public class HelloYou {
+  public static void main(String[] args) {
+    System.out.println("Hello Kyra!");
+  }
+}
