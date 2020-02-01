714487a768a2496ae11150b1e95a2fc1f0e0bca6
Kyra Mellema <kemellema@gmail.com>
2020-02-01 11:59:55 -0500

    Practice from 2/1/20

--- codecademyJava.java
+++ codecademyJava.java
@@ -1,5 +1,6 @@
 // “Write once, run everywhere”
 
+// Introduction to Java
 // Run the code in the text editor to see what is printed to the screen.
 public class HelloWorld {
   public static void main(String[] args) {
@@ -7,9 +8,68 @@ public class HelloWorld {
   }
 }
 
+// Hello Java File!
 // Inside main(), add a statement which prints Hello someName!, with your name replacing someName.
 public class HelloYou {
   public static void main(String[] args) {
     System.out.println("Hello Kyra!");
   }
 }
+
+// Commenting Code
+// The file Timeline.java has plain text information about Java. Plain text facts aren’t valid syntax. We’ll use comments to avoid breaking the program. Use the single-line comment syntax for the first fact and the multi-line syntax for the second.
+public class Timeline {
+  public static void main(String[] args) {
+    System.out.println("Hello Java!");
+    
+    System.out.println("You were born in 1995");
+
+    // Sun Microsystems announced the release of Java in 1995
+    
+    System.out.println("You were created by James Gosling");
+    
+		/* James Gosling is a Canadian engineer who 
+		created Java while working at Sun Microsystems. 
+		His favorite number is the square root of 2! */
+    
+    System.out.println("You are a fun language!");
+  }
+} 
+
+// Semicolons and Whitespace
+// This file prints information about Java to the screen. Unfortunately, the writer of the file has avoided using whitespace. Make the file easier to read by adding a newline after each statement! Inside main(), add a new statement printing how you feel about coding.
+public class LanguageFacts {
+  public static void main(String[] args) {
+    System.out.println("Programming is amazing!");
+    
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+  }
+}
+
+// Java Review: Putting It All Together
+// The text editor holds an empty file named Review.java. Fill it in! Define a public class named Review. Define the main() method. Inside the main() method, write "The main method executes the tasks of the class" as a single-line comment. Below the comment, write a statement that prints the following: My first Java program from scratch!.
+public class Review{
+  public static void main(String[] args){
+    // The main method executes the tasks of the class
+    System.out.println("My first Java program from scratch!")
+  }
+}
+
+// Planting a Tree
+// Define a class that follows the Java naming conventions. Define the main() method inside of the Tree class. Write a comment to explain the goal then use a print statement to introduce yourself. After introducing yourself, use another print statement to output the project goal! Use multiple print statements to plant your tree.
+public class Tree{
+  public static void main(String[] args){
+    // I'm going to introduce myself and print a tree to the screen.
+    System.out.println("My name is Kyra and I'm learning Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("  *** ");
+    System.out.println(" *****");
+    System.out.println(" *****");
+    System.out.println("   | ");
+    System.out.println("   | ");
+    System.out.println("^^^^^^^");
+  }
+}
+
